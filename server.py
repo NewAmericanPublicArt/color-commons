82adbc02194a2629c27a674f50874f0a145c691e
Brandon Stafford <brandon@pingswept.org>
2017-05-10 17:27:46 -0400

    Add pseudocode for new features from Dan

--- server.py
+++ server.py
@@ -78,32 +78,86 @@ def parse_sms():
     wrapper.Run()
     return ('<?xml version="1.0" encoding="UTF-8" ?><Response></Response>')
 
-# @public.route('/sms', methods=['POST'])
-# def control_lights():
-#     d = {'airforceblue': '11',
-#         'airsuperiorityblue': '11',
-#         'aliceblue': '16',
-#         'youcompletemelights': '92',
-#         'zebra': '93'}
-#     allowed_commands = ['X040A',
-#         'X040B',
-#         'X0462']
-#     import random
-#     message = request.form['Body']
-#     print "Received text message: " + str(message)
-#     try:
-#         program = int(d[message[0:25].lower().replace(' ', '')])
-#     except KeyError:
-#         print 'color {0} not found'.format(message)
-#         program = random.randint(10,98)
-#     command = 'X04%(number)2.2X' % {"number": program}
-#     print 'Translated {0} to {1}'.format(message, command)
-#     if (command in allowed_commands):
-#         pytronics.serialWrite(command, speed=9600)
-#     else:
-#         print "Command {0} is not one of the allowed commands.".format(command)
-#         command = 'FAIL'
-#     return('<?xml version="1.0" encoding="UTF-8"?><Response>{0}</Response>'.format(command))
+	# Rainbow across all blades
+	# Simple. Everyone wants this and loves it. 
+	# I like when the two lights on each blade are different creating a little variance across the blade itself. 
+
+
+
+	# (this next one is the stretch goal. But represents a how new data set of user experimentation.)
+
+	# Modifier  
+	# Use the word 'secret' as a modifier. 
+	# I've written in some code that will get you thinking, for better or worse.
+
+	# Example text : 'secret turquoise'
+
+	# Result : lights show turquoise and the inverse of turquoise, alternating between lights.
+	# (the same way secret does red and blue alternating between lights)
+
+	# How : 
+	# 1. The code checks if the message begins with the word secret
+
+	# 2. if 'secret' is only the word then just play the standard alternating color secret show.
+	  
+	# 3. if there is a word after 'secret' then try the second word in the data base
+
+	# 4. use the new color hex in an inverse color function : Linked here (shown below)
+
+	# 5. Alternate between lights with the two colors.
+
+
+	# This is a great site that can show you what some of these inverses are : http://coloreminder.com/
+
+
+	# Here's my quick cut and paste psudo code
+
+	# # Sum of the min & max of (a, b, c) 
+	# def hilo(a, b, c): 
+	# if c < b: b, c = c, b 
+	# if b < a: a, b = b, a 
+	# if c < b: b, c = c, b 
+	# return a + c 
+
+	# def complement(r, g, b): 
+	# k = hilo(r, g, b) 
+	# return tuple(k - u for u in (r, g, b))
+
+	# if(message.startswith("secret")):
+	# if len(message.split()) > 1:
+	# try:
+	# color = webcolors.hex_to_rgb(xkcd_names_to_hex[message.split(" ")[1]])
+	# except:
+	# color = [randint(0, 255), randint(0, 255), randint(0, 255)]
+
+	# # this part is above my pay grade. Need a var colorInverse with new rgb values in it's array.
+	# # need to use the above complement function, but don't know how
+
+	# data.append(color[0])
+	# data.append(color[1])
+	# data.append(color[2])
+	# data.append(colorInverse[0])
+	# data.append(colorInverse[1])
+	# data.append(colorInverse[2])
+	# data = data * (num_fixtures/2)
+
+	# else
+	# data.append(0)
+	# data.append(0)
+	# data.append(255)
+	# data.append(255)
+	# data.append(0)
+	# data.append(0)
+	# data = data * (num_fixtures/2)
+	# else:
+	# try:
+	# color = webcolors.hex_to_rgb(xkcd_names_to_hex[message])
+	# except:
+	# color = [randint(0, 255), randint(0, 255), randint(0, 255)]
+	# data.append(color[0])
+	# data.append(color[1])
+	# data.append(color[2])
+	# data = data * num_fixtures
 
 if __name__ == "__main__":
     public.run(host='127.0.0.1:5000', debug=True)
